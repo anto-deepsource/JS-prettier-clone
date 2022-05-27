61c4d351a2c1f4c2a1421f4804a35d569cf613b1
manhhung012 <nhoxwiz87@gmail.com>
2022-05-27 11:12:52 +0700

    init

---
+++ scripts/vendors/vendors.mjs
@@ -0,0 +1,16 @@
+const vendors = [
+  "chalk",
+  "css-units-list",
+  "escape-string-regexp",
+  "execa",
+  "html-element-attributes",
+  "html-tag-names",
+  "html-void-elements",
+  "leven",
+  "mem",
+  "string-width",
+  "strip-ansi",
+  "tempy",
+];
+
+export default vendors;
